56c7e82005b1180f6393b3d4086f1e211416fa30
moonq <moongq@gmail.com>
2021-04-25 15:59:37 +0900

    Graphql NestJS typeORM RDS boilerplate

--- src/loaders/database/database.providers.ts
+++ src/loaders/database/database.providers.ts
@@ -5,11 +5,11 @@ export const databaseProviders = [
     provide: 'DATABASE_CONNECTION',
     useFactory: async () => await createConnection({
       type: 'mysql',
-      host: 'database-1.cbzpsfvcb7i0.ap-northeast-2.rds.amazonaws.com',
+      host: '',
       port: 3306,
-      username: 'admin',
-      password: 'chldbflehdrmfkal',
-      database: 'name0421',
+      username: '',
+      password: '',
+      database: '',
       entities: [
           __dirname + '/../../**/*.model{.ts,.js}',
       ],
