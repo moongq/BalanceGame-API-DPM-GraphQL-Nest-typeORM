8f999bfb46f30d6761391e6486028809e584b902
lemontech119 <lemontech119@gmail.com>
2021-05-19 14:17:58 +0900

    Update/밸런스 게임 리스트

--- src/balance-game/dto/balance-game-state.input.ts
+++ src/balance-game/dto/balance-game-state.input.ts
@@ -1,10 +1,10 @@
 import { InputType, Field } from "@nestjs/graphql";
 
 @InputType()
-export class BalanceGamesState {
-  @Field()
+export class BalanceGamesStateInput {
+  @Field({description: "밸런스 게임 나오는 갯수"})
   limit: number;
 
-  @Field()
+  @Field({description: "skip할 밸런스 게임 갯수(현재 웹에 나와있는 밸런스 게임 갯수 넣으시면 됩니다."})
   offset: number;
 }
